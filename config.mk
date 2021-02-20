ee980b34621365d327448490485091378385149c
afify <hassan@afify.dev>
2021-02-20 20:16:44 +0300

    v0.2

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # azan version
-VERSION = 0.1
+VERSION = 0.2
 
 # paths
 PREFIX    = /usr/local
@@ -9,7 +9,7 @@ MANPREFIX = ${PREFIX}/share/man
 AFLAGS  = -f elf64 -w+all -D$$(uname) -DVERSION=\"${VERSION}\"
 LFLAGS  = -m elf_x86_64 -s -no-pie
 # FreeBSD (uncomment)
-# LFLAGS  = -m elf_amd64_fbsd -s
+#LFLAGS  = -m elf_amd64_fbsd -s
 
 # assembler and linker
 ASM  = nasm
